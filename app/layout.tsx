069ed9d082a53cd517f88546079746d542a121f8
sagarddsij <sagard@dsij.in>
2024-09-12 17:04:14 +0530

    Auth0 integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <UserProvider>
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </UserProvider>
     </html>
   );
 }
