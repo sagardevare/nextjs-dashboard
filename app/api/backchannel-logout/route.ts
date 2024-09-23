fceae08ba365780d609d2f5eb780a608cbf659c4
sagarddsij <sagard@dsij.in>
2024-09-23 17:51:01 +0530

    Added backchannel-logout and checksession api routes

---
+++ app/api/backchannel-logout/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from 'next/server';
+//import validateLogoutToken from '@/app/lib/validatelogouttoken';
+//import { deleteUserSessions } from '@/utils/sessions';
+
+export async function POST(req:NextRequest) {
+  try {
+    console.log("backchannel endpoint hit...")
+    const body = await req.json();
+    
+    console.log(body);
+    const logouttoken = req?.body?.logout_token;
+    console.log(logouttoken);
+    
+    // Call the middleware to validate the logout token
+     const res = new NextResponse();
+    /* await new Promise((resolve, reject) => {
+      validateLogoutToken(req, res, (err: any) => {
+        if (err) return reject(err);
+        resolve(null);
+      });
+    });  */
+
+    // Access the logout token
+    const { logoutToken } = (req as any);
+    
+    // Delete user session using `sub` or `sid`
+    //await deleteUserSessions(logoutToken.sub, logoutToken.sid);
+    console.log("Session deleted")
+
+    // Return success response
+    return NextResponse.json({ message: 'User logged out successfully' }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
---
+++ app/api/check-session/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+//import { getSession } from '@auth0/nextjs-auth0';
+
+/* export async function GET(req: Request) {
+  // Use Auth0 to get the session
+  const session = await getSession();
+    console.log(session)
+  /* if (!session) {
+    // No session found, respond with 401 and a logout URL
+    return NextResponse.json({ redirectTo: '/api/auth/logout' }, { status: 401 });
+  } */
+
+  // Session exists, respond with a success message
+  //return NextResponse.json({ message: 'Session active' });
+//} */
+
+import { getSession } from '@auth0/nextjs-auth0/edge';
+
+const GET = async function GET(req) {
+  const res = new NextResponse();
+  const session = await getSession();
+  console.log(session?.accessTokenExpiresAt);
+  session.user.sid = "destroyed";
+  return NextResponse.json(session?.user);
+};
+
+export { GET };
